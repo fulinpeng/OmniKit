e85c13555c1636d5b878c8336ec3791406ea9020
fulinpeng <fulp511@gmail.com>
2025-03-27 12:00:00 +0800

    git

--- gitTools/git.sh
+++ gitTools/git.sh
@@ -4,5 +4,12 @@ git config --get user.email
 git config user.name "xxx"
 git config user.email "xxx@xxx.com"
 
+git config --list
+git stash save "xxx"
+git stash list
+git stash pop
+git stash drop
+git stash clear
+git stash apply
 
 GIT_AUTHOR_DATE="2025-03-15T12:00:00" GIT_COMMITTER_DATE="2025-03-15T12:00:00" git commit -m "xxx"
