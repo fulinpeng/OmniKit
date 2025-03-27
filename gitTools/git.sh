dd3e318b56532af21a98a2d08efc21b9e175c680
fulinpeng <fulp511@gmail.com>
2025-03-27 12:00:00 +0800

    git

--- gitTools/git.sh
+++ gitTools/git.sh
@@ -1,5 +1,8 @@
 git config --get user.name
 git config --get user.email
 
+git config user.name "xxx"
+git config user.email "xxx@xxx.com"
+
 
 GIT_AUTHOR_DATE="2025-03-15T12:00:00" GIT_COMMITTER_DATE="2025-03-15T12:00:00" git commit -m "xxx"
